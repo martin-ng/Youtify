1fa165842fc2e72cc3a4c2aaffaa2f7ff5304abe
Martin Ng <martinng1992@gmail.com>
2020-03-22 01:21:25 -0400

    fixed downloaded oauth secrets

--- create_playlists.py
+++ create_playlists.py
@@ -1,6 +1,6 @@
-
 import json
 import requests
+
 from secrets import spotify_client_id, spotify_secret
 
 import os
@@ -9,31 +9,39 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-class Playlists:
+
+class Playlists():
     def __init__(self):
         self.client_id = spotify_client_id
-        self.youtube_client_id = self.get_youtube_client
+        self.youtube_client = self.get_youtube_client
+        print(self.youtube_client)
+        print(self.client_id)
 
     def get_youtube_client(self):
-
-        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
         api_service_name = "youtube"
         api_version = "v3"
-        client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+        # Download from Youtube Oauth
+        client_secrets_file = "CLIENT_SECRETS.json"
+
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
 
+
         credentials = flow.run_console()
 
+        # Youtube Web API Docs
         youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
-
         return youtube
 
-    def create_playlist(self):
+    def get_youtube_liked_videos(self):
+        request = self.youtube_client.videos().list(part="snippet,contentDetails,statistics", myRating="like")
+        response = request.execute()
 
+    def create_playlist(self):
         request_body = json.dumps({
             "name": "Videos",
             "description": "Liked Youtube Videos",
@@ -52,7 +60,11 @@ class Playlists:
 
         # print("query: ", query)
 
+
 # Testing functions
 
 new_playlist = Playlists()
-new_playlist.create_playlist()
+
+# new_playlist.create_playlist()
+# new_playlist.get_youtube_client()
+# new_playlist.get_youtube_liked_videos()
